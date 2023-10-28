ca96280dbffd1042a74fcb2e8f17b91600b20556
Darren Embry <dse@webonastick.com>
2023-10-28 19:52:55 -0400

    prep work for supporting different pixel types

--- src/pixeltrace.h
+++ src/pixeltrace.h
@@ -1,6 +1,12 @@
 #ifndef PIXELTRACE_H
 #define PIXELTRACE_H
 
+enum {
+     PIXEL_TYPE_SQUARE   = 1,
+     PIXEL_TYPE_SCANLINE = 2,
+     PIXEL_TYPE_CIRCLE   = 3
+};
+
 int pixel_trace(char* filename);
 
 #endif
