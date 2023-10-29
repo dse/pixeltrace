9cd518292fe24a4b13a4cc1511e1045824654207
Darren Embry <dse@webonastick.com>
2023-10-28 22:01:32 -0400

    comments

--- src/pixeltrace.h
+++ src/pixeltrace.h
@@ -2,8 +2,8 @@
 #define PIXELTRACE_H
 
 enum {
-     PIXEL_TYPE_NONE      = 0,  /* none selected */
-     PIXEL_TYPE_PLAIN     = 1,
+     PIXEL_TYPE_NONE      = 0,  /* no pixel type selected */
+     PIXEL_TYPE_PLAIN     = 1,  /* straight up plain pixel fonts */
      PIXEL_TYPE_RECTANGLE = 2,  /* mainly for LCD display module lookin' fonts */
      PIXEL_TYPE_SCANLINE  = 3,  /* mainly for CRT lookin' fonts */
      PIXEL_TYPE_CIRCLE    = 4   /* more for dot matrix printing */
